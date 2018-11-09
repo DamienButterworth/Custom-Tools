99587e7863252567ed9183699b504a6543229686
DamienButterworth <u1252462@unimail.hud.ac.uk>
2018-11-09 21:48:14 +0000

    Add files via upload

---
+++ mongo-drop-all.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env mongo
+var dbs = db.getMongo().getDBNames();
+for(var i in dbs){
+    db = db.getMongo().getDB( dbs[i] );
+    print( "dropping db " + db.getName() );
+    db.dropDatabase();
+}
